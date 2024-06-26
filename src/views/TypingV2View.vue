5710155a519018e446945cb31ecc2e7cf783376d
Moufid Agbannonde <magbannonde@gmail.com>
2024-06-26 17:30:00 +0200

    timer

--- src/views/TypingV2View.vue
+++ src/views/TypingV2View.vue
@@ -1,4 +1,27 @@
+<script setup>
+
+let min = 0;
+let sec = 0;
+
+function timing() {
+    const intervalId = setInterval(() => {
+        if(sec < 10){
+            sec = "0" + sec;
+            }
+      if(sec === 60){
+            min++;
+            sec = 0;
+        } 
+    },1000)
+    // console.log(intervalId);
+}
+</script>
+
 <template>
-    
-    
+    <div id="">
+        
+    </div>
 </template>
+<style scoped>
+
+</style>
