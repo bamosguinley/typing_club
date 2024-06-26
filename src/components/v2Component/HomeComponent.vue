f2da72b079c524e39df45078eb483ae8d95eab80
Giovanni <giovanni.sokenou2105@gmail.com>
2024-06-26 20:08:26 +0200

    restart

--- src/components/v2Component/HomeComponent.vue
+++ src/components/v2Component/HomeComponent.vue
@@ -1,8 +1,10 @@
 <script setup>
 import TimerComponent from '../v2Component/TimerComponent.vue';
-    function compareUser(params) {
-        
-    }
+  
+        const refreshPage = ()=>{
+          location.reload();
+        }
+
 </script>
 <template>
      
@@ -10,12 +12,12 @@ import TimerComponent from '../v2Component/TimerComponent.vue';
           <slot></slot>
      </div>
      <div class="restart">
-               <RouterLink to="/version2">
+               <a href="#" @click="refreshPage">
                     <svg xmlns="http://www.w3.org/2000/svg" height="60px" viewBox="0 -960 960 960" width="60px" fill="#df7132">
                          <path
                               d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z" />
                     </svg>
-               </RouterLink>
+               </a>
           </div>
 </template>
 <script setup>
