2f3add69011cbfb1dabc5934d2e5d47785b0be44
Bamos Guinley <guinleyb@gmail.com>
2024-06-26 19:04:21 +0200

    mise à jour du landing et gestion de l'affichage des mots a taper

--- src/components/v2Component/LandingComponent.vue
+++ src/components/v2Component/LandingComponent.vue
@@ -1,52 +1,33 @@
-<script setup>
-</script>
-
 <template>
-  <div class="container">
-    <div class="text-container">
-      <p class="text">Tester votre vitesse de saisie au clavier</p>
-      <button class="btn">Commencer</button>
-    </div>
-    <div class="img-conontainer">
-      <img src="/imgcontainer.gif" alt="Home Image" />
-    </div>
+  <div class="home">
+      <img src="/typing.gif" alt="">
+    <h1>Bienvenue sur notre site de typing</h1>
+    <slot>
+
+    </slot>
   </div>
 </template>
 
 <style scoped>
-.container {
-  display: flex;
-  justify-content: space-around;
-  padding: 3rem;
-  align-items: center;
-}
-.text-container {
+.home {
   display: flex;
   flex-direction: column;
-  padding: 1rem;
+  align-items: center;
+  margin: 0;
+  justify-content: center;
+  height: 90vh;
+  background-color: #f0f0f0; /* Couleur de fond */
+  font-family: Arial, Helvetica, sans-serif; /* Police de caractères */
+  overflow-y: hidden;
 }
-.btn {
-  padding: 1rem;
-  width: 12rem;
-  text-align: center;
-  border: none;
-  border-radius: 1rem;
-  background-color: #4cb7d8;
-  color: #fff;
-  font-size: 1rem;
-  text-decoration: none;
+
+h1 {
+  font-size: 2em;
+  margin-bottom: 20px;
 }
-.btn:hover {
-  background-color: #2c81c7;
-}
-.text{
-  font-size: 5rem;
-  font-weight: bold;
-  font-family:poppins, sans-serif;
-}
-@media screen and (max-width: 600px) {
-    .img-conontainer{
-        display: none;
-    }
+
+
+button:hover {
+  background-color: #0056b3; /* Changement de couleur au survol */
 }
 </style>
--- src/views/TypingV2View.vue
+++ src/views/TypingV2View.vue
@@ -1,41 +1,51 @@
 <script setup>
-function compareUser(params) {}
+import HomeComponent from "@/components/v2Component/HomeComponent.vue";
+import LandingComponent from "@/components/v2Component/LandingComponent.vue";
+import { ref } from "vue";
+const showText = ref(false);
 </script>
 <template>
-  <div class="container">
-    <p class="text">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
-      temporibus alias veniam laboriosam quos provident reprehenderit ducimus
-      eveniet soluta sit nobis at maiores praesentium commodi mollitia, nostrum
-      quaerat sunt vero consectetur deleniti libero numquam consequatur magnam
-      accusamus? Voluptatem accusantium et magnam blanditiis fuga neque omnis
-      aperiam quia? Minima dolorum mollitia sunt delectus exercitationem
-      reiciendis ut impedit at magnam perspiciatis labore repellendus doloribus
-      ex, facilis deserunt placeat eligendi aut iusto atque, laudantium omnis
-      vitae illo quae. Rem non vitae architecto laudantium, explicabo id ipsum
-      ea natus dicta labore quo modi. Labore maiores vero dolorem quidem, a
-      accusamus cum dicta nisi atque?
-    </p>
+  <div>
+    <HomeComponent v-if="showText">
+      <div class="container">
+        <p class="text">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+          temporibus alias veniam laboriosam quos provident reprehenderit
+          ducimus eveniet soluta sit nobis at maiores praesentium commodi
+          mollitia, nostrum quaerat sunt vero consectetur deleniti libero
+          numquam consequatur magnam accusamus? Voluptatem accusantium et magnam
+          blanditiis fuga neque omnis aperiam quia? Minima dolorum mollitia sunt
+          delectus exercitationem reiciendis ut impedit at magnam perspiciatis
+          labore repellendus doloribus ex, facilis deserunt placeat eligendi aut
+          iusto atque, laudantium omnis vitae illo quae. Rem non vitae
+          architecto laudantium, explicabo id ipsum ea natus dicta labore quo
+          modi. Labore maiores vero dolorem quidem, a accusamus cum dicta nisi
+          atque?
+        </p>
+      </div>
+    </HomeComponent>
+    <landing-component v-else>
+      <button @click="showText = true">Commencer</button>
+    </landing-component>
   </div>
 </template>
-<script setup>
-</script>
 <style scoped>
-/* Style pour le contenu du texte */
-.container {
-  max-width: 1000px;
-  width: 100%;
-  height: 300px;
-  margin: 2rem auto;
-  border: 1px solid red;
-  background-color: #fff;
-  padding: 2rem;
-  overflow: clip;
-}
 /* style pour le text  */
 .text {
   font-size: 2rem;
   font-family: Verdana, Geneva, Tahoma, sans-serif;
   line-height: 1.5;
+  text-align: justify;
+  opacity: 0.5;
+}
+button {
+  padding: 10px 20px;
+  font-size: 1.2em;
+  cursor: pointer;
+  background-color: #007bff; /* Couleur de fond du bouton */
+  color: white; /* Couleur du texte du bouton */
+  border: none;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
 }
 </style>
