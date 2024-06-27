23c657dcccc419287afd5eb90951014259906057
Eliette christelle vanie quenum <quenumeliette@gmail.com>
2024-06-27 10:05:55 +0100

    fonction local storage

--- src/composable/utils.js
+++ src/composable/utils.js
@@ -18,6 +18,40 @@ export function getWord(position, nombreMot) {
 
 getWord(5, 10);
 
+
+
+const myWords = ["chat", "chien", "oiseau", "fleur"];
+
+
+
+function getRandomWord(wordsArray) {
+  const randomIndex = Math.floor(Math.random() * wordsArray.length);
+  return wordsArray[randomIndex];
+}
+ 
+
+// Appel de la fonction au chargement de la page
+window.addEventListener("load", () => {
+  const randomWord = getRandomWord(myWords);
+  console.log("Mot aléatoire au chargement de la page :", randomWord);
+});
+
+
+function storeRandomWord(word) {
+  localStorage.setItem("randomWord", word);
+}
+
+// Appel de la fonction lors de la génération du mot aléatoire
+const newRandomWord = getRandomWord(myWords);
+storeRandomWord(newRandomWord);
+
+
+window.addEventListener("load", () => {
+  const storedRandomWord = localStorage.getItem("randomWord");
+  console.log("Mot aléatoire précédent :", storedRandomWord);
+});
+
+
 export function setString(String) {
 
 }
