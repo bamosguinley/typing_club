00b2a8fa3bc9069675d28ddec2dd2b0685a2aafa
Aristideoronkponde <legroscapo@gmail.com>
2024-07-01 09:04:19 +0100

    up

--- src/components/v2Component/HomeComponent.vue
+++ src/components/v2Component/HomeComponent.vue
@@ -1,8 +1,7 @@
 <script setup>
-
 import TimerComponent from "../v2Component/TimerComponent.vue";
 import SpinnerComponent from "../v2Component/SpinnerComponent.vue";
-import { onBeforeMount, onMounted, reactive, ref,watch } from "vue";
+import { onBeforeMount, onMounted, reactive, ref, watch } from "vue";
 import {
   getObject,
   getPrecision,
@@ -17,7 +16,7 @@ const refreshPage = () => {
   location.reload();
 };
 let word = ref([]);
-let start= ref(false)
+let start = ref(false);
 let wordObject = ref([]); //Initialiser un tableau d'objet mot
 onBeforeMount(() => {
   word.value = getWord(50);
@@ -39,11 +38,9 @@ onMounted(() => {
     });
   });
   setTimeout(() => {
-    return start.value = true;
-  },2000)
-  
+    return (start.value = true);
+  }, 2000);
 });
-
 
 const typingCount = ref(0);
 const counting = ref(false);
@@ -106,9 +103,9 @@ function Input(e) {
           wrongCount.value = 0;
         }
       } else if (e.key === "Backspace" && letterCounter.value > 0) {
-      /**
-       * Retourner en arrière en cas d'erreur
-       */
+        /**
+         * Retourner en arrière en cas d'erreur
+         */
         wordObject.value[wordCounter.value].isFinding = "";
         letterCounter.value--;
         wrongCount.value--;
@@ -145,69 +142,72 @@ watch(
   () => timeIsUp.value,
   () => {
     vitesse = getSpeed(wordCounter.value, 3);
-     const attemps= wordObject.value.map((el) => el.wrongPerWord)
-    const totalCaract = wordObject.value.map((el) => el.mot.length).reduce((acc, el) => el + acc, 0)
-     console.log(totalCaract);
+    const attemps = wordObject.value.map((el) => el.wrongPerWord);
+    const totalCaract = wordObject.value
+      .map((el) => el.mot.length)
+      .reduce((acc, el) => el + acc, 0);
+    console.log(totalCaract);
     totalWrong = attemps.reduce((acc, el) => el + acc, 0);
-    precision = getPrecision(totalCaract,totalWrong);
+    precision = getPrecision(totalCaract, totalWrong);
     localStorage.clear();
   }
-
 );
-
 </script>
 <template>
-<div class="global">
-  <div class="loader" v-if="start===false">
-  <spinner-component />
-</div>
-<div class="contenu" v-else>
-    <div class="container" v-if="timeIsUp === false">
-    <TimerComponent v-if="counting" @sendTimeOver="(el) => (timeIsUp = el)" />
-    <span
-      class="text"
-      v-for="(word, index) in wordObject"
-      :key="index"
-      :class="{
-        writeWord: word.isFinding === 'vrai',
-        wrongWord: word.isFinding === 'faux',
-        currentW: word.isCurrent === true,
-      }"
-    >
-      <span
-        class="letterSpan"
-        v-for="(letter, index1) in word.mot.split('')"
-        :key="index1"
-        :class="{
-          green: index === wordCounter && index1 === letterCounter ,
-        }"
-      >
-        {{ letter }}
-      </span>
-    </span>
-  </div>
-  <ResultComponent
-    v-if="timeIsUp === true"
-    :vitesseProps="vitesse"
-    :precisionProps="precision"
-  />
-  <div class="restart">
-    <a href="">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        height="60px"
-        viewBox="0 -960 960 960"
-        width="60px"
-        fill="#df7132"
-      >
-        <path
-          d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z"
+  <div class="global">
+    <div class="loader" v-if="start === false">
+      <spinner-component />
+    </div>
+    <div class="contenu" v-else>
+      <div class="container" v-if="timeIsUp === false">
+        <TimerComponent
+          v-if="counting"
+          @sendTimeOver="(el) => (timeIsUp = el)"
         />
-      </svg>
-    </a>
+        <span
+          class="text"
+          v-for="(word, index) in wordObject"
+          :key="index"
+          :class="{
+            writeWord: word.isFinding === 'vrai',
+            wrongWord: word.isFinding === 'faux',
+            currentW: word.isCurrent === true,
+          }"
+        >
+          <span
+            class="letterSpan"
+            v-for="(letter, index1) in word.mot.split('')"
+            :key="index1"
+            :class="{
+              green: index === wordCounter && index1 === letterCounter,
+            }"
+          >
+            {{ letter }}
+          </span>
+        </span>
+      </div>
+      <ResultComponent
+        v-if="timeIsUp === true"
+        :vitesseProps="vitesse"
+        :precisionProps="precision"
+      />
+      <div class="restart">
+        <a href="">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="60px"
+            viewBox="0 -960 960 960"
+            width="60px"
+            fill="#df7132"
+          >
+            <path
+              d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
   </div>
-</div>
-</div>
 </template>
 <style scoped>
 /* .global{
@@ -237,7 +237,7 @@ watch(
   background-color: transparent;
   padding: 3rem;
   margin-bottom: 2rem;
-  overflow:clip;
+  overflow: clip;
 }
 .restart {
   margin-top: 10rem;
